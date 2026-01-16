8981cca86baa20c65d7e8b8c71d5ae76d3610a2d
DimaLyahanov <zerozero1488322@gmail.com>
2026-01-16 20:03:53 +0300

    HW 1 done sosat

---
+++ SearchTest.java
@@ -0,0 +1,8 @@
+public class SearchTest {
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+}
